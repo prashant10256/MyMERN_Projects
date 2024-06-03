c80ed77a192b6be809420900f1e2486184808812
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-06-04 01:06:23 +0530

    Final Commit

--- BookStore App/Frontend/src/components/Banner.jsx
+++ BookStore App/Frontend/src/components/Banner.jsx
@@ -29,7 +29,7 @@ function Banner() {
               <input type="text" className="grow" placeholder="Email" />
             </label>
           </div>
-          <button className="btn btn-secondary mt-6">Secondary</button>
+          <button className="btn btn-secondary mt-6">Get Started</button>
         </div>
         <div className="order-1 w-full md:w-1/2">
           <img 
