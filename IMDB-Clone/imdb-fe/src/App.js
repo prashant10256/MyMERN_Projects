6ca065d6b658c6566ddbb794cc117d58ea942e6f
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-06-07 14:52:34 +0530

    Create Components

--- IMDB-Clone/imdb-fe/src/App.js
+++ IMDB-Clone/imdb-fe/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Movie from './pages/home/movieDetail/Movie';
+import MovieList from './components/movieList/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
-        <Route index element={<h1>Hello world 2</h1>}></Route>
-        <Route path="movies/:id" element={<h1>Movie detail page</h1>}></Route>
-        <Route path="movies/:type" element={<h1>Movies list page</h1>}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="movie/:id" element={<Movie />}></Route>
+        <Route path="movies/:type" element={<MovieList/>}></Route>
         <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
       </Router>
