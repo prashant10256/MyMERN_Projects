e6465afd4604da01576a70ee24bcfb48a13791dd
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-06-16 01:57:36 +0530

    Create Router

--- Restaurant Apllication/BackEnd/models/reservationSchema.js
+++ Restaurant Apllication/BackEnd/models/reservationSchema.js
@@ -35,4 +35,4 @@ const reservationSchema = new mongoose.Schema({
   },
 });
 
-export const Reservation = mongoose.model("Reservation",reservationSchema )
+export const Reservation = mongoose.model("Reservation", reservationSchema )
