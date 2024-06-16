373959f44a65a5a0dd37b0e8c1854f047ff4f138
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-06-17 02:14:36 +0530

    Final Commit

--- Restaurant Apllication/BackEnd/models/reservationSchema.js
+++ Restaurant Apllication/BackEnd/models/reservationSchema.js
@@ -5,34 +5,34 @@ const reservationSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    minLength: [3, "First name must contain at least 3 characters!"],
-    maxlength: [30, "First name can not exceed 30 characters!"],
+    minLength: [3, "First name must be of at least 3 Characters."],
+    maxLength: [30, "First name cannot exceed 30 Characters."],
   },
   lastName: {
     type: String,
     required: true,
-    minLength: [3, "First name must contain at least 3 characters!"],
-    maxlength: [30, "First name can not exceed 30 characters!"],
-  },
-  email: {
-    type: String,
-    required: true,
-    validate: [validator.isEmail, "Provide a valid email!"],
-  },
-  phone: {
-    type: String,
-    required: true,
-    minLength: [10, "phone number must contain only 10 digits!"],
-    maxlength: [10, "phone number must contain only 10 digits!"],
-  },
-  time: {
-    type: String,
-    required: true,
+    minLength: [3, "Last name must be of at least 3 Characters."],
+    maxLength: [30, "Last name cannot exceed 30 Characters."],
   },
   date: {
     type: String,
     required: true,
   },
+  time: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Provide a valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength: [10, "Phone number must contain 10 Digits."],
+    maxLength: [10, "Phone number must contain 10 Digits."],
+  },
 });
 
-export const Reservation = mongoose.model("Reservation", reservationSchema )
+export const Reservation = mongoose.model("Reservation", reservationSchema);
