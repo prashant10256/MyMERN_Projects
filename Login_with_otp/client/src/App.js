78234a6ace290936ade6588ac80eb444ba57cebe
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-06-22 02:01:15 +0530

    UI Development

--- Login_with_otp/client/src/App.js
+++ Login_with_otp/client/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import Login from "./pages/Login"
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Otp from "./pages/Otp";
+import Error from "./pages/Error";
+import Headres from "./components/Headres";
+import { Routes, Route,  } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import './App.css';
 
 function App() {
   return (
     <>
-    <h1>hello...</h1>
+    <Headres/>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/user/otp' element={<Otp/>}/>
+      <Route path='/*' element={<Error/>}/>
+
+    </Routes>
     </>
   );
 }
--- Login_with_otp/client/src/pages/Login.js
+++ Login_with_otp/client/src/pages/Login.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "../styles/mix.css";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <section>
+        <div className="form_data">
+          <div className="form_heading">
+            <h1>Welcome Back, Log In</h1>
+            <p>Hi, We are you glad you are back. Please login.</p>
+          </div>
+          <form>
+            <div className="form_input">
+              <lable htmlFor="email">Email</lable>
+              <input
+                type="email"
+                name="email"
+                id=""
+                placeholder="Enter your email address"
+              ></input>
+            </div>
+            <button className="btn">Login</button>
+            <p>Dont't have and account <NavLink to="/register">Sign up</NavLink></p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Login
+export default Login;
